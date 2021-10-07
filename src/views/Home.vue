8a2caf8e919123c72b876a6679f8556fb2c76b21
errr4567 <ega108242424@gmail.com>
2021-10-07 19:22:24 +0300

    sfgzdfg
    ardsg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,6 +29,40 @@
         </table>
       </div>
     </div>
+    <b-modal ref="addBookModal"
+         id="book-modal"
+         title="Add a new book"
+         hide-footer>
+  <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+  <b-form-group id="form-title-group"
+                label="Title:"
+                label-for="form-title-input">
+      <b-form-input id="form-title-input"
+                    type="text"
+                    v-model="addBookForm.title"
+                    required
+                    placeholder="Enter title">
+      </b-form-input>
+    </b-form-group>
+    <b-form-group id="form-author-group"
+                  label="Author:"
+                  label-for="form-author-input">
+        <b-form-input id="form-author-input"
+                      type="text"
+                      v-model="addBookForm.author"
+                      required
+                      placeholder="Enter author">
+        </b-form-input>
+      </b-form-group>
+    <b-form-group id="form-read-group">
+      <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+        <b-form-checkbox value="true">Read?</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+  </b-form>
+</b-modal>
   </div>
 </template>
 
@@ -39,6 +73,11 @@ export default {
   data() {
     return {
       notes: [],
+       addNoteForm: {
+        Title: '',
+        Body: '',
+        CreateTime: '',
+      },
     };
   },
   methods: {
@@ -48,12 +87,47 @@ export default {
         .then((res) => {
           this.notes = res.data;
         })
-        .catch((error) => {
+        .catch((err) => {
           // eslint-отключение следующей строки
-          console.error(error);
+          console.error(err);
         });
     },
   },
+  addNote(payload) {
+      const path = 'http://localhost:50898/api/Notes';
+      axios.post(path, payload)
+        .then(() => {
+          this.getNotes();
+        })
+        .catch((error) => {
+          // eslint-отключение следующей строки
+          console.log(error);
+          this.getNotes();
+        });
+    },
+    initForm() {
+      this.addNoteForm.Title = '';
+      this.addNoteForm.Body = '';
+      this.addNoteForm.CreateTime = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addNoteModal.hide();
+      let read = false;
+      if (this.addNoteForm.read[0]) read = true;
+      const payload = {
+        Title: this.addNoteForm.Title,
+        Body: this.addNoteForm.Body,
+        CreateTime:this.addNoteForm.CreateTime, // сокращённое свойство
+      };
+      this.addNote(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addNoteModal.hide();
+      this.initForm();
+    },
   created() {
     this.getNotes();
   },
